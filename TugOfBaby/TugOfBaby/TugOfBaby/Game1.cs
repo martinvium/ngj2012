be38aa63ed053636b32e492d837d4c896bbea5e7
lashdl <lashdl@gmail.com>
2012-01-28 14:45:17 +0100

    spriteBatch.begin() error

--- TugOfBaby/TugOfBaby/TugOfBaby/Game1.cs
+++ TugOfBaby/TugOfBaby/TugOfBaby/Game1.cs
@@ -218,7 +218,7 @@ namespace TugOfBaby
             }
             
 
-            spriteBatch.Begin();
+            
             theBackground.Draw(spriteBatch);
             spriteBatch.End();
             base.Draw(gameTime);
